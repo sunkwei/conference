6c07d2ba5fa37877d1665132e2e9eb4803bd84cf
sunkw <sunkwei@gmail.com>
2014-05-09 20:49:56 +0800

    wsapi:   新增 video source/sink, audio stream, members, conference 操作接口

--- mcu/gsoap/mcu.interface.h
+++ mcu/gsoap/mcu.interface.h
@@ -55,23 +55,89 @@ typedef struct mcu__MediaEndpoint
 // Source 描述，一个 Source 对应着一个视频点播源
 typedef struct mcu__VideoSource
 {
-    int id  :1;     // 唯一id，大于 0 有效
+    int sourceid  :1;     // 全局唯一id，大于 0 有效
     int codec :1;   // 编码类型，总是 0，对应 h264
 
     struct mcu__MediaEndpoint endpoint :1;
 } mcu__VideoSourceResponse;
 
-int mcu__addSource(char *memberid, mcu__VideoSourceResponse &res);
-int mcu__delSource(char *memberid, char *sourceid, void *);
+struct mcu__VideoSourceArray
+{
+    struct mcu__VideoSource *__ptr :1;
+    int __size :1;
+};
+
+int mcu__addVideoSource(int confid, int memberid, mcu__VideoSourceResponse &res); // 根据 res.sourceid 判断是否成功
+int mcu__delVideoSource(int confid, int memberid, int sourceid, void *);
 
 // Sink 描述，一个 Sink 对应着一个视频点播的接收端
 typedef struct mcu__VideoSink
 {
-    int id :1;      // 唯一 id，大于0有效
+    int sinkid :1;      // 全局唯一 id，大于0有效
     int sourceid :1;    // 点播的 source
   
     struct mcu__MediaEndpoint endpoint :1;
 } mcu__VideoSinkResponse;
 
-int mcu__addSink(char *memberid, mcu__VideoSinkResponse &res);
-int mcu__delSink(char *memberid, char *sinkid, void *);
+struct mcu__VideoSinkArray
+{
+    struct mcu__VideoSink *__ptr;
+    int __size :1;
+};
+
+int mcu__addVideoSink(int confid, int memberid, int sourceid, mcu__VideoSinkResponse &res);
+int mcu__delVideoSink(int confid, int memberid, int sinkid, void *);
+
+// Stream, 一个 stream 为双向 rtp，目前仅仅用于音频 ...
+typedef struct mcu__AudioStream
+{
+    int streamid :1;    // 全局唯一id，大于0有效
+    
+    struct mcu__MediaEndpoint endpoint :1;
+    
+} mcu__AudioStreamResponse;
+
+int mcu__addAudioStream(int confid, int memberid, mcu__AudioStreamResponse &res);
+int mcu__delAudioStream(int confid, int memberid, int streamid, void *);
+
+// Member ，描述一个成员，包含数个 Source 和 Sink
+typedef struct mcu__Member
+{
+    int memberid :1;      // 唯一id，大于 0 有效
+    char *name :1;  // 显示名字？？
+    
+    struct mcu__VideoSourceArray sources :1;
+    struct mcu__VideoSinkArray sinks :1;
+    struct mcu__AudioStream audio :1;
+    
+    int enableaudio :1;         // 是否启用音频 ...
+} mcu__MemberResponse;
+
+struct mcu__MemberArray
+{
+    struct mcu__Member *__ptr :1;
+    int __size :1;
+};
+
+int mcu__addMember(int confid, mcu__MemberResponse &res);   // 通过 res.memberid 判断是否成功
+int mcu__delMember(int confid, int memberid, void *);
+int mcu__enableSound(int confid, int memberid, int enable, int &res);   // res 是否成功
+
+// Conference, 描述一个会议，由一堆 Member 构成
+typedef struct mcu__Conference
+{
+    int confid : 1;         // 会议唯一标识，大于0有效
+    char *desc : 1;         // 会议描述？？
+    
+    struct mcu__MemberArray members :1; // 会议成员
+} mcu__ConferenceResponse;
+
+typedef struct mcu__ConferenceArray
+{
+    struct mcu__Conference *__ptr :1;
+    int __size :1;
+} mcu__ConferenceArrayResponse;
+
+int mcu__addConference(void *, mcu__ConferenceResponse &res);   // 通过 res.confid 判断是否成功
+int mcu__delConference(int confid, void *);			// 删除 confid 对应的会议
+int mcu__listConferences(void *, mcu__ConferenceArrayResponse &res);    // 返回所有会议 ..
--- mcu/mcu/WsImpl.cpp
+++ mcu/mcu/WsImpl.cpp
@@ -44,3 +44,63 @@ int mcu__getStatus(soap *soap, void *, mcu__StatusResponse &res)
     
     return SOAP_OK;
 }
+
+int mcu__addVideoSource(soap *soap, int conf_id, int member_id, mcu__VideoSourceResponse &res)
+{
+    return SOAP_OK;
+}
+
+int mcu__delVideoSource(soap *soap, int conf_id, int member_id, int source_id)
+{
+    return SOAP_OK;
+}
+
+int mcu__addVideoSink(soap *soap, int conf_id, int member_id, int source_id, mcu__VideoSinkResponse &res)
+{
+    return SOAP_OK;
+}
+
+int mcu__delVideoSink(soap *soap, int conf_id, int member_id, int sink_id)
+{
+    return SOAP_OK;
+}
+
+int mcu__addAudioStream(soap *soap, int conf_id, int member_id, mcu__AudioStreamResponse &res)
+{
+    return SOAP_OK;
+}
+
+int mcu__delAudioStream(soap *soap, int conf_id, int member_id, int streamid)
+{
+    return SOAP_OK;
+}
+
+int mcu__addMember(soap *soap, int conf_id, mcu__MemberResponse &res)
+{
+    return SOAP_OK;
+}
+
+int mcu__delMember(soap *soap, int conf_id, int member_id)
+{
+    return SOAP_OK;
+}
+
+int mcu__enableSound(soap *soap, int conf_id, int member_id, int enable, int &res)
+{
+    return SOAP_OK;
+}
+
+int mcu__addConference(soap *soap, void *, mcu__ConferenceResponse &res)
+{
+    return SOAP_OK;
+}
+
+int mcu__delConference(soap *soap, int conf_id)
+{
+    return SOAP_OK;
+}
+
+int mcu__listConferences(soap *soap, void *, mcu__ConferenceArrayResponse &res)
+{
+    return SOAP_OK;
+}
